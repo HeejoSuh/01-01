dbc86a3be65e735266b2876b8bc08b0161764610
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-02-09 13:28:43 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/****************************************************************************
+ *
+ * Created by: Heejo Suh
+ * Created on: Feb 2018
+ * Created for: learning
+ * 
+ * This program prints Hello World
+ *     
+ *
+ ****************************************************************************/
+
+
+
+//****************************************************************************
+public class HelloWorld{
+
+    public static void main(String[] args) {
+    	System.out.print("Hello World");
+
+    }//closing for public static void main
+}//closing for public class HelloWorld
